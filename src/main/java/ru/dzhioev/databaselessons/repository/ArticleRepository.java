0e153147cfe946de8377a1ce18251164b2867151
nitropasha <djioeff@gmail.com>
2025-05-25 11:07:13 +0500

    Коммит

--- src/main/java/ru/dzhioev/databaselessons/repository/ArticleRepository.java
+++ src/main/java/ru/dzhioev/databaselessons/repository/ArticleRepository.java
@@ -1,5 +1,6 @@
 package ru.dzhioev.databaselessons.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,8 +11,11 @@ import java.util.List;
 @Repository
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    @Query("SELECT a FROM Article a JOIN FETCH a.authors")
+    @Query("SELECT DISTINCT a FROM Article a JOIN FETCH a.authors")
     List<Article> findAllArticles();
+
+//    @EntityGraph(attributePaths = {"authors"})
+//    List<Article> findAll();
 
 
 }
